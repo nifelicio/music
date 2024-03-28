223eeab2a766c0660933cdbad952661314215790
niifelicio <ds.44842225823@gmail.com>
2024-03-28 09:30:46 -0300

    first commit

---
+++ src/components/MusicItem/index.js
@@ -0,0 +1,35 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import styles from "./styles";
+
+
+export default function MusicItem({
+    music,
+    onPlayPause,
+    IsPlaying,
+    navigation
+}) {
+    return (
+        <View style={styles.musicItem}>
+            <Image source={{ uri: music.album_image }} style={styles.albumImage} />
+            <View style={styles.musicInfo}>
+                <TouchableOpacity onpress={() => { }}>
+                    <View>
+                        <Text style={IsPlaying ? styles.playingTitle : styles.musicTitle}>
+                            {music.title}
+                        </Text>
+                        <Text style={styles.musicGroup}>{music.group}</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onpress={onPlayPause}>
+                    <FontAwesome
+                        name={IsPlaying ? "pause" : "play"}
+                        color="#fff"
+                        size={16}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import MusicItem from "../components/MusicItem";
+
+export default function Home({ navigation }) {
+  const [currentPlaying, setCurrentPlaying] = useState(null);
+  const item = {
+    id: 1,
+    title: "Young and beautiful",
+    group: "AC/DC",
+    album_image:
+      "https://i.discogs.com/zjMkOcLUu2f3jJ0CzZrZa_5ZszuIajRbM8NhPm6ZWAg/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc0Mjcw/OTUtMTQ0MTI5OTQ2/NC03NjUyLnBuZw.jpeg"
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#121212" />
+      <Text style={styles.title}>Minhas Músicas</Text>
+      <MusicItem
+        IsPlaying={() => currentPlaying == item.id}
+        music={item}
+        navigation={navigation}
+        onPlayPause={() => { }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    paddinTop: 16,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 20,
+    marginLeft: 20,
+
+
+
+  },
+})
+
+
+
+
+
+
+
+
