fc3f2e95f31b89d9062eed5523e45d78eabed206
niifelicio <ds.44842225823@gmail.com>
2024-04-11 11:25:19 -0300

    projeto final

--- src/components/MusicItem/index.js
+++ src/components/MusicItem/index.js
@@ -1,39 +1,85 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import styles from "./styles";
 
 
 export default function MusicItem({
     music,
     onPlayPause,
-    IsPlaying,
-    navigation
+    navigation,
+    isPlaying,
 }) {
     return (
         <View style={styles.musicItem}>
-            <Image 
-            source={{ uri: `http://10.0.2.2:3000/assets/${music.album_image}` }}
-            style={styles.albumImage} 
+            <Image
+                source={{
+                    uri: `http://10.0.2.2:3000/assets/${music.album_image}`,
+                }}
+                style={styles.albumImage}
             />
-
             <View style={styles.musicInfo}>
-                <TouchableOpacity onpress={() => { }}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("Details", { musicData: music })}
+                >
                     <View>
-                        <Text style={IsPlaying ? styles.playingTitle : styles.musicTitle}>
+                        <Text style={isPlaying ? styles.playingTitle : styles.musicTitle}>
                             {music.title}
                         </Text>
                         <Text style={styles.musicGroup}>{music.group}</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity
-                    onpress={onPlayPause}>
+                <TouchableOpacity onPress={onPlayPause}>
                     <FontAwesome
-                        name={IsPlaying ? "pause" : "play"}
-                        color="#fff"
+                        name={isPlaying ? "pause" : "play"}
                         size={16}
+                        color="white"
                     />
                 </TouchableOpacity>
             </View>
         </View>
-    )
+    );
 }
+const styles = StyleSheet.create({
+    musicItem: {
+        backgroundColor: "#212121",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginVertical: 4,
+        marginLeft: 20,
+        marginRight: 20,
+        borderRadius: 4,
+    },
+    musicItemButton: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    albumImage: {
+        width: 72,
+        height: 72,
+        borderRadius: 4,
+    },
+    musicInfo: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginHorizontal: 20,
+        flex: 1,
+    },
+    musicTitle: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "700",
+    },
+    playingTitle: {
+        color: "#1DB954",
+        fontSize: 20,
+        fontWeight: "700",
+    },
+    musicGroup: {
+        color: "#b3b3b3",
+        fontSize: 16,
+    },
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,59 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
+import { Audio } from "expo-av";
 import MusicItem from "../components/MusicItem";
 
+
 export default function Home({ navigation }) {
+  const [musicData, setMusicData] = useState([]);
   const [currentPlaying, setCurrentPlaying] = useState(null);
- const [musicData, setMusicData] = useState([]);
-  const item = {
-    id: 1,
-    title: "Young and beautiful",
-    group: "AC/DC",
-    album_image:
-      "https://i.discogs.com/zjMkOcLUu2f3jJ0CzZrZa_5ZszuIajRbM8NhPm6ZWAg/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc0Mjcw/OTUtMTQ0MTI5OTQ2/NC03NjUyLnBuZw.jpeg",
-    album: "Lazy Sunday 3",
-    year: 2013,
-    genre: "Pop",
+  const [currentSound, setCurrentSound] = useState(null);
+
+
+  const togglePlayPause = async (item) => {
+    if (currentSound && currentPlaying === item.id) {
+      await currentSound.pauseAsync();
+      setCurrentPlaying(null);
+      setCurrentSound(null);
+    } else {
+      if (currentSound) {
+        await currentSound.unloadAsync();
+      }
+      const { sound } = await Audio.Sound.createAsync(
+        { uri: `http://10.0.2.2:3000/musics/${item.music_path}` },
+        { shouldPlay: true }
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
     }
+  };
 
-  useEffect(()=> {
+
+  useEffect(() => {
     fetch("http://10.0.2.2:3000/musics")
-   .then((response)=>response.json())
-    .then((data) =>setMusicData(data));
-  },[])
+      .then((response) => response.json())
+      .then((data) => setMusicData(data));
+  }, []);
 
- 
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#121212" />
       <Text style={styles.title}>Minhas Músicas</Text>
-      
 
 
-         <FlatList
-         data={musicData}
-         keyExtractor={(item)=> item.id.toString()}
-         renderItem={({item}) => (
+      <FlatList
+        data={musicData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
           <MusicItem
-          IsPlaying={() => currentPlaying == item.id}
-          music={item}
-          navigation={navigation}
-          onPlayPause={() => { }}
-         /> 
-         )} 
-         />
-
-
-
+            music={item}
+            onPlayPause={() => togglePlayPause(item)}
+            isPlaying={currentPlaying === item.id}
+            navigation={navigation}
+          />
+        )}
+      />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121212",
-    paddinTop: 16,
+    paddingTop: 16,
   },
   title: {
     fontSize: 36,
@@ -61,16 +71,8 @@ const styles = StyleSheet.create({
     color: "#fff",
     marginBottom: 20,
     marginLeft: 20,
-
-
-
   },
-})
-
-
-
-
-
+});
 
 
 
